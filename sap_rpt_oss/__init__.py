330863cfccf21f5f861bd5d56b8bd86e30ba5d66
Maximilian Schambach <28300359+MaxSchambach@users.noreply.github.com>
2025-11-04 08:57:30 +0100

    Rename and update

--- sap_rpt_oss/__init__.py
+++ sap_rpt_oss/__init__.py
@@ -2,6 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from contexttab.contexttab import ConTextTabClassifier, ConTextTabRegressor
+from sap_rpt_oss.rpt import SAP_RPT_OSS_Classifier, SAP_RPT_OSS_Regressor
 
-__all__ = ['ConTextTabClassifier', 'ConTextTabRegressor']
+__all__ = ['SAP_RPT_OSS_Classifier', 'SAP_RPT_OSS_Regressor']
